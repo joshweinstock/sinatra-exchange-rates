e199d91528c2ec42b0c3194a335abb6aa3d17f1f
joshweinstock <jyweinstock@uchicago.edu>
2025-02-07 02:50:57 +0000

    exch_rate5

--- app.rb
+++ app.rb
@@ -8,24 +8,29 @@ get("/") do
   currency_url= "https://api.exchangerate.host/list?access_key="+ exch_key
   @raw_currency_response= HTTP.get(currency_url).to_s
   @parse_currency=JSON.parse(@raw_currency_response)
-  @currency_array = @parse_currency.fetch("currencies")
-  @currency_hash = {
-    from_abrev: @currency_array.keys,
-    currency: @currency_array.values
-  }
+  @currency_array = @parse_currency.fetch("currencies").keys
 
 erb(:homepage)
 end
 
 get("/:from_abrev")do
-@orginal_currency=params.fetch("from_abrev")
+@original_currency=params.fetch("from_abrev")
 api_url= "https://api.exchangerate.host/list?access_key="+exch_key
-@raw_currency_response= HTTP.get(currency_url).to_s
+@raw_currency_response= HTTP.get(api_url).to_s
 @parse_currency=JSON.parse(@raw_currency_response)
 @currency_array = @parse_currency.fetch("currencies")
-@currency_hash = {
-  to_abrev: @currency_array.keys,
-  currency: @currency_array.values
-}
+@currency = @currency_array.keys
 
+erb(:from_abrev)
+end
+
+get("/:from_abrev/:to_abrev")do
+@original_currency=params.fetch("from_abrev")
+@destination_currency=params.fetch("to_abrev")
+exchange_url= "https://api.exchangerate.host/convert?access_key=" +exch_key +"&from=#{@original_currency}&to=#{@destination_currency}&amount=1"
+@raw_currency_response= HTTP.get(exchange_url).to_s
+@parse_currency=JSON.parse(@raw_currency_response)
+@currency_array = @parse_currency.fetch("quotes")
+@quote = @parse_currency.fetch(@destination_currency)
+erb(:to_abrev)
 end
